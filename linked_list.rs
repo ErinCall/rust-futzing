d4513e2393d98bb6a111fc26fe7fe834c0a01f28
Andrew Lorente <hello@andrewlorente.com>
2014-09-20 13:33:57 -0700

    Simple linked list that can tell you its length

--- linked_list.rs
+++ linked_list.rs
@@ -1,14 +1,22 @@
 enum List<T> {
-    Node(Box<T>, Box<List<T>>),
+    Node(T, Box<List<T>>),
     Null
 }
 
-// impl<T> List<T> {
-//     fn cons <T>(&self, next: &T) -> &List<T> {
-//         &Node(next, self)
-//     }
-// }
+impl<T> List<T> {
+    fn length(&self) -> int {
+        match *self {
+            Null => 0,
+            Node(_, ref next) => 1i + next.length(),
+        }
+    }
+}
 
 fn main() {
+    let empty:List<int> = Null;
+    let one = Node(8i, box Null);
 
+    assert!(empty.length() == 0);
+    assert!(one.length() == 1);
+    println!("Hurray!")
 }
